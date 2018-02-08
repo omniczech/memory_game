815e407dc71da7a85af47616f8b2364c1dc53478
Michael Petschek <mikepetschek@gmail.com>
2018-02-08 12:00:39 -0500

    added game logic from unit 8 assignment

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["Queen","Queen","King","King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }
+  else{
+    alert("Sorry, try again.");
+  }
+}
